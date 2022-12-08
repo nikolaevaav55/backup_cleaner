14c274b209227c419b5454ee849a612728c7956c
nastia <nikolaevaav55@gmail.com>
2022-12-08 13:30:28 +0300

    Path to directory fixed

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
 import datetime
+import pathlib
 
 
-PATH = "/var/www/html/rldipacademy/dbrldump/"
-MAX_LENGTH = 3
+DIRECTORY_NAME = 'dbrldump'
+PATH = pathlib.Path(__file__).parent / f"{DIRECTORY_NAME}"
+MAX_FILE_LIMIT = 3
 
 
 def delete_old_backup(path=PATH):
     dirs_list = os.listdir(path)
-    while len(dirs_list) > MAX_LENGTH:
+    while len(dirs_list) > MAX_FILE_LIMIT:
         try:
             my_list = sorted([x[8:24] for x in dirs_list])
             min_date = min(my_list, key=lambda i: datetime.datetime.strptime(i, '%Y-%m-%d-%H-%M'))
-            os.remove(f"{PATH}dbrldip-{min_date}.sql")
+            os.remove(f"{PATH}/dbrldip-{min_date}.sql")
             dirs_list = os.listdir(path)
             print("Удалена папка: ", min_date)
         except Exception as error:
